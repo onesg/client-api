3e3f4603c7d64234bcfaa97f472fb911d513f75e
onesg <gabriel.seno@fatec.sp.gov.br>
2022-06-11 15:40:30 -0300

    Adicionando validação a ClientController

--- src/main/java/br/com/clientapi/clientapi/rest/ClientController.java
+++ src/main/java/br/com/clientapi/clientapi/rest/ClientController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/clients")
 public class ClientController {
@@ -14,39 +16,39 @@ public class ClientController {
     private final ClientRepository repository;
 
     @Autowired
-    public ClientController(ClientRepository repository){
+    public ClientController(ClientRepository repository) {
         this.repository = repository;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Client insert(@RequestBody Client client){
+    public Client insert(@RequestBody @Valid Client client) {
         return repository.save(client);
     }
 
     @GetMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Client findById(@PathVariable Integer id){
+    public Client findById(@PathVariable Integer id) {
         return repository.findById(id)
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void delete(@PathVariable Integer id){
+    public void delete(@PathVariable Integer id) {
         repository.findById(id).map(client -> {
             repository.delete(client);
             return Void.TYPE;
-        }).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
     }
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@PathVariable Integer id, @RequestBody Client clientUpdated){
+    public void update(@PathVariable Integer id, @RequestBody @Valid Client clientUpdated) {
         repository.findById(id).map(client -> {
             clientUpdated.setIdClient(client.getIdClient());
             return repository.save(clientUpdated);
-        }).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
     }
 
 }
