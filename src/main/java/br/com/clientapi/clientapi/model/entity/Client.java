a8202abfe6c7b5f3afe922bd43d70842f67dc121
onesg <gabriel.seno@fatec.sp.gov.br>
2022-06-11 14:40:09 -0300

    Adicionado arquivo messages.properties e validação @NotEmpty e @NotNull

--- src/main/java/br/com/clientapi/clientapi/model/entity/Client.java
+++ src/main/java/br/com/clientapi/clientapi/model/entity/Client.java
@@ -5,8 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -21,9 +24,12 @@ public class Client {
     private Integer idClient;
 
     @Column(name = "name_client", nullable = false, length = 300)
+    @NotEmpty(message = "{field.name.required}")
     private String nameClient;
 
     @Column(name = "cpf_client", nullable = false, length = 11)
+    @NotNull(message = "{field.cpf.required}")
+    @CPF(message = "{field.cpf.invalid}")
     private String cpfClient;
 
     @Column(name = "date_register_client", nullable = false, updatable = false)
